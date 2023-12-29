7514e0313b417104ca82334b8109ffaecfff73f6
Tiberiu Deak <tiberiudeak09@gmail.com>
2023-12-29 14:42:10 +0200

    int the file-table menu, a file name can be typed and it will be loaded from memory and executed; still needs cleaning/refactoring

--- src/boot_sector2.asm
+++ src/boot_sector2.asm
@@ -11,6 +11,7 @@ xor ax, ax		; set DS to 0 (data segment)
 mov ds, ax
 cld
 
+
 ;; ===============================================================
 ;; READ FILE TABLE INTO MEMORY (0x7e00)
 ;; ===============================================================
@@ -29,19 +30,30 @@ int 0x13		; BIOS interrupt for disk functions
 ;; READ KERNEL INTO MEMORY (0x7f00)
 ;; ===============================================================
 mov ah, 0x02	; int 13h function 2
-mov al, 0x02	; we want to read 2 sectors
+mov al, 0x03	; we want to read 3 sectors
 mov ch, 0x0		; cylinder 0
-mov cl, 0x03	; start with the second sector
+mov cl, 0x03	; start with the third sector
 mov dh, 0x0		; head number 0
 
 xor bx, bx		; set BX to 0
 mov es, bx		; set ES to 0
-mov bx, 0x7f00	; and BX to 0x7f00 (EX:BX = 0x7f00)
+mov bx, 0x8000	; and BX to 0x7f00 (EX:BX = 0x7f00)
 int 0x13		; BIOS interrupt for disk functions
 
 jc disk_error	; if carry flag is set, there was an error
+; mov ah, 0x02				; set up for int 0x13
+; 	mov al, 0x01				; number of sectors to read
+; 	mov ch, 0x00				; cylinder number
+; 	mov dh, 0x00				; head number
+; 	mov cl, 0x06					; sector number
 
-jmp 0x7f00		; jump to kernel sector
+; 	xor bx, bx					; set BX to 0
+; 	mov es, bx					; load segment address into ES
+; 	mov bx, 0x5000				; load offset address into BX
+
+; 	int 0x13
+; jc disk_error	; if carry flag is set, there was an error
+jmp 0x8000		; jump to kernel sector
 
 disk_error:
 	mov bx, disk_error_message	; load error message into BX
@@ -52,6 +64,7 @@ disk_error_message:
 	db "Disk read error!",0
 
 %include "./print/print_string.asm"
+%include "./print/print_hex.asm"
 
 times 510-($-$$) db 0
 dw 0xaa55
--- src/kernel.asm
+++ src/kernel.asm
@@ -1,4 +1,4 @@
-org 0x7f00
+org 0x8000
 
 main_menu:
 	;; reset screen state
@@ -97,6 +97,167 @@ filebrowser:
 	mov bx, 0x7e00
 	call print_filetable
 
+	mov bx, file_table_message
+	call print_string
+
+get_filename:
+	mov di, cmd_string			; cmd_string will store the command
+	mov byte [cmd_length], 0	; reset cmd_length
+
+filename_loop:
+	xor ah, ah					; set AX to 0
+	int 0x16					; BIOS int get keystroke, character goes into al
+
+	mov ah, 0x0e				; BIOS int 1-h teletype output
+	mov bh, 0x00
+
+	cmp al, 0xD					; did user press 'enter'
+	je search_file
+	int 0x10					; BIOS int to print character in AL
+	mov [di], al				; store value from DI in AL
+	add di, 1					; increment DI
+	inc byte [cmd_length]		; increment cmd_length
+	jmp filename_loop
+
+
+
+
+search_file:
+	;; search for the filename in the file table
+	;; filetable is stored in memory location 0x7e00
+	;; and has the format: {filename-sector,filename-sector,...}
+
+	mov bx, 0x7e00				; load filetable into SI
+	mov si, 0
+	mov di, cmd_string			; load filename into DI
+
+
+search_loop:
+	mov al, [bx+si]				; load byte from filetable into AL
+	cmp al, '}'
+	je file_not_found
+
+	; mov al, si				; load byte from filetable into AL
+	cmp al, [di]				; compare byte from filetable with byte from filename
+
+	je match					; if equal, jump to match
+	add si, 1					; if not equal, increment SI
+	jmp search_loop				; and loop
+
+match:
+	mov cl, [cmd_length]		; load cmd_length into CL
+
+	; mov dx, cx
+	; call print_hex				; print cmd_length as hex number
+
+	dec cl						; decrement CL
+
+match_loop:
+
+	inc di						; increment DI
+	inc si						; increment SI
+
+	mov al, [bx+si]				; load byte from filetable into AL
+	cmp byte al, [di]				; compare byte from filetable with byte from filename
+
+	jne restart_search
+
+	dec cl						; decrement CL
+	cmp cl, 0					; check if CL is 0
+	je file_found				; if CL is 0, jump to file_found
+
+	jmp match_loop				; else, loop
+
+restart_search:
+
+	mov di, cmd_string			; load filename into DI
+	jmp search_loop				; restart search
+
+file_found:
+
+	inc si						; increment SI (skip "-" character)
+	inc si
+	mov bx, filename_found
+	call print_string
+
+	mov bx, 0x7e00				; load filetable into BX
+
+	mov cl, 10					; load 10 into CL
+	xor al, al					; set AL to 0
+	xor dx, dx
+
+sector_number_loop:
+	mov dl, [bx+si]				; load byte from filetable into DL
+
+	inc si						; increment SI
+	cmp dl, ','					; end of sector number
+	je load_file				; if so, jump to load_file
+	cmp dl, '}'					; end of filetable
+	je load_file				; if so, jump to load_file
+	cmp dl, 48					; check if DL is less than 0
+	jl sector_not_found			; if so, jump to sector_not_found
+	cmp dl, 57					; check if DL is greater than 9
+	jg sector_not_found			; if so, jump to sector_not_found
+	sub dl, 48					; convert from ASCII to decimal
+	mul cl						; multiply AL by CL
+	add al, dl					; add DL to AL
+	jmp sector_number_loop		; loop
+
+sector_not_found:
+	mov bx, sect_not_found
+	call print_string
+	mov ah, 0x00	; get user keystroke
+	int 0x16
+	jmp main_menu
+
+load_file:
+	clc
+	mov cl, al					; load sector number into CL
+
+	; mov ah, 0x00				; set up for int 0x13
+	; mov dl, 0x00				; drive number
+	; int 0x13					; reset disk
+
+
+
+	mov ah, 0x02				; set up for int 0x13
+	mov al, 0x01				; number of sectors to read
+	mov ch, 0x00				; cylinder number
+	mov dh, 0x00				; head number
+	mov cl, 0x06					; sector number
+	mov dl, 0x80
+
+	xor bx, bx					; set BX to 0
+	mov es, bx					; load segment address into ES
+	mov bx, 0x9000				; load offset address into BX
+
+	int 0x13					; read sector
+	jnc load_success			; if no error, jump to load_success
+
+
+	mov bx, program_load_fail
+	call print_string
+	mov ah, 0x00	; get user keystroke
+	int 0x16
+	jmp main_menu
+
+load_success:
+	xor bx, bx					; set BX to 0
+	mov es, bx					; load segment address into ES
+	mov bx, 0x9000			; load segment address into AX
+	; mov ds, ax					; load segment address into DS
+	; mov es, ax					; load segment address into ES
+	; mov ss, ax					; load segment address into SS
+	; mov fs, ax					; load segment address into FS
+	; mov gs, ax					; load segment address into GS
+	jmp 0x9000			; jump to loaded program
+
+file_not_found:
+	mov bx, filename_not_found
+	call print_string
+
+temp_end:
+
 	mov bx, go_back
 	call print_string
 
@@ -161,8 +322,25 @@ newline:
 pr_reg_header:
 	db "Register       Value", 0xA, 0xD, \
 	"--------       -----", 0xA, 0xD, 0
+file_table_message:
+	db 0xA, 0xD, "Enter program to load: ", 0
+
+cmd_length:
+	db 0
+
+filename_found:
+	db "Filename found", 0xA, 0xD, 0
+
+filename_not_found:
+	db "Filename not found", 0xA, 0xD, 0
+
+sect_not_found:
+	db "Sector not found", 0xA, 0xD, 0
+
+program_load_fail:
+	db "Program load failed", 0xA, 0xD, 0
 
 cmd_string:
 	db ''
 
-times 1024-($-$$) db 0
+times 1536-($-$$) db 0
