ca902048b3743fd1fee731e4249c25a19da78424
Tiberiu Deak <tiberiudeak09@gmail.com>
2024-01-08 14:35:54 +0200

    update Makefile to include 2nd stage boot

--- src/kernel/kernel.d
+++
@@ -1,5 +0,0 @@
-kernel.o: kernel.c \
- /home/tibi/opt/cross/lib/gcc/i686-elf/11.4.0/include/stdbool.h \
- /home/tibi/opt/cross/lib/gcc/i686-elf/11.4.0/include/stddef.h \
- /home/tibi/opt/cross/lib/gcc/i686-elf/11.4.0/include/stdint.h \
- /home/tibi/opt/cross/lib/gcc/i686-elf/11.4.0/include/stdint-gcc.h
