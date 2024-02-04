62f26c7521bc8e24cc70824d50ccc81c154ff552
Tiberiu Deak <tiberiudeak09@gmail.com>
2024-02-04 12:10:54 +0200

    the bootloader now initializes the stack to address 0x7c00 because the previous location was overriden by the kernel code when loaded from disk
    add interrupts: still a problem because when enabled, qemu has the exception invalid tss type, so probably I need to look over the gdt and add a tss segment

--- kernel/arch/i386/idt.c
+++ kernel/arch/i386/idt.c
@@ -1,19 +1,175 @@
-#include "idt.h"
+#include <kernel/idt.h>
+#include <kernel/io_port.h>
+#include <string.h>
+#include <stdio.h>
 
-idt_gate_t idt[256];
+idt_gate_t idt_entries[256];
+idt_ptr_t idt_ptr_reg;
 
 /**
- * @brief Set the IDT gate.
+ * @brief Set the idt_entries gate.
  *
- * This function sets the IDT gate.
+ * This function sets the idt_entries gate.
  *
- * @param n        The number of the IDT gate.
- * @param handler  The handler of the IDT gate.
+ * @param n        The number of the idt_entries gate.
+ * @param handler  The handler of the idt_entries gate.
  */
 void set_idt_gate(int n, uint32_t handler) {
-	idt[n].low_offset = low_16(handler);
-	idt[n].selector = KERNEL_CS;
-	idt[n].always0 = 0;
-	idt[n].flags = 0x8E;
-	idt[n].high_offset = high_16(handler);
+	idt_entries[n].low_offset = low_16(handler);
+	idt_entries[n].selector = KERNEL_CS;
+	idt_entries[n].always0 = 0;
+	idt_entries[n].flags = 0x8E;
+	idt_entries[n].high_offset = high_16(handler);
+}
+
+void init_idt() {
+	printf("Initializing IDT...\n");
+
+	idt_ptr_reg.limit = (sizeof(idt_gate_t) * 256) - 1;
+	idt_ptr_reg.base = (uint32_t)&idt_entries;
+
+	memset(&idt_entries, 0, sizeof(idt_gate_t) * 256);
+
+	port_byte_out(0x20, 0x11);
+	port_byte_out(0xA0, 0x11);
+
+	port_byte_out(0x21, 0x20);
+	port_byte_out(0xA1, 0x28);
+
+	port_byte_out(0x21, 0x04);
+	port_byte_out(0xA1, 0x02);
+
+	port_byte_out(0x21, 0x01);
+	port_byte_out(0xA1, 0x01);
+
+	port_byte_out(0x21, 0x0);
+	port_byte_out(0xA1, 0x0);
+
+
+	set_idt_gate(0, (uint32_t)isr0);
+	set_idt_gate(1, (uint32_t)isr1);
+	set_idt_gate(2, (uint32_t)isr2);
+	set_idt_gate(3, (uint32_t)isr3);
+	set_idt_gate(4, (uint32_t)isr4);
+	set_idt_gate(5, (uint32_t)isr5);
+	set_idt_gate(6, (uint32_t)isr6);
+	set_idt_gate(7, (uint32_t)isr7);
+	set_idt_gate(8, (uint32_t)isr8);
+	set_idt_gate(9, (uint32_t)isr9);
+	set_idt_gate(10, (uint32_t)isr10);
+	set_idt_gate(11, (uint32_t)isr11);
+	set_idt_gate(12, (uint32_t)isr12);
+	set_idt_gate(13, (uint32_t)isr13);
+	set_idt_gate(14, (uint32_t)isr14);
+	set_idt_gate(15, (uint32_t)isr15);
+	set_idt_gate(16, (uint32_t)isr16);
+	set_idt_gate(17, (uint32_t)isr17);
+	set_idt_gate(18, (uint32_t)isr18);
+	set_idt_gate(19, (uint32_t)isr19);
+	set_idt_gate(20, (uint32_t)isr20);
+	set_idt_gate(21, (uint32_t)isr21);
+	set_idt_gate(22, (uint32_t)isr22);
+	set_idt_gate(23, (uint32_t)isr23);
+	set_idt_gate(24, (uint32_t)isr24);
+	set_idt_gate(25, (uint32_t)isr25);
+	set_idt_gate(26, (uint32_t)isr26);
+	set_idt_gate(27, (uint32_t)isr27);
+	set_idt_gate(28, (uint32_t)isr28);
+	set_idt_gate(29, (uint32_t)isr29);
+	set_idt_gate(30, (uint32_t)isr30);
+	set_idt_gate(31, (uint32_t)isr31);
+
+	set_idt_gate(32, (uint32_t)irq0);
+	set_idt_gate(33, (uint32_t)irq1);
+	set_idt_gate(34, (uint32_t)irq2);
+	set_idt_gate(35, (uint32_t)irq3);
+	set_idt_gate(36, (uint32_t)irq4);
+	set_idt_gate(37, (uint32_t)irq5);
+	set_idt_gate(38, (uint32_t)irq6);
+	set_idt_gate(39, (uint32_t)irq7);
+	set_idt_gate(40, (uint32_t)irq8);
+	set_idt_gate(41, (uint32_t)irq9);
+	set_idt_gate(42, (uint32_t)irq10);
+	set_idt_gate(43, (uint32_t)irq11);
+	set_idt_gate(44, (uint32_t)irq12);
+	set_idt_gate(45, (uint32_t)irq13);
+	set_idt_gate(46, (uint32_t)irq14);
+	set_idt_gate(47, (uint32_t)irq15);
+
+	set_idt_gate(128, (uint32_t)isr128);
+	set_idt_gate(177, (uint32_t)isr177);
+
+	__asm__ __volatile__("lidt %0" : : "m" (idt_ptr_reg));
+	__asm__ __volatile__("sti");
+}
+
+char *exception_messages[] = {
+	"Division By Zero",
+	"Debug",
+	"Non Maskable Interrupt",
+	"Breakpoint",
+	"Into Detected Overflow",
+	"Out of Bounds",
+	"Invalid Opcode",
+	"No Coprocessor",
+	"Double Fault",
+	"Coprocessor Segment Overrun",
+	"Bad TSS",
+	"Segment Not Present",
+	"Stack Fault",
+	"General Protection Fault",
+	"Page Fault",
+	"Unknown Interrupt",
+	"Coprocessor Fault",
+	"Alignment Check",
+	"Machine Check",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved"
+};
+
+void isr_handler(interrupt_regs *r) {
+	if (r->int_no < 32) {
+		printf("Received interrupt: ");
+		printfc(4, "%s", exception_messages[r->int_no]);
+		printf("\n");
+		printf("System Halted!\n");
+		for (;;);
+	}
+}
+
+void *irq_routines[16] = {
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0
+};
+
+void irq_install_handler(int irq, void (*handler)(interrupt_regs *r)) {
+	irq_routines[irq] = handler;
+}
+
+void irq_uninstall_handler(int irq) {
+	irq_routines[irq] = 0;
+}
+
+void irq_handler(interrupt_regs *r) {
+	void (*handler)(interrupt_regs *r);
+	handler = irq_routines[r->int_no - 32];
+	if (handler) {
+		handler(r);
+	}
+
+	if (r->int_no >= 40) {
+		port_byte_out(0xA0, 0x20);
+	}
+	port_byte_out(0x20, 0x20);
 }
--- kernel/include/kernel/idt.h
+++ kernel/include/kernel/idt.h
@@ -1,5 +1,5 @@
-#ifndef ARCH_I386_INT_H
-#define ARCH_I386_INT_H 1
+#ifndef ARCH_I386_IDT_H
+#define ARCH_I386_IDT_H 1
 
 #include <stdint.h>
 
@@ -50,4 +50,75 @@ typedef struct {
 	uint16_t high_offset;
 } __attribute__((packed)) idt_gate_t;
 
-#endif // ARCH_I386_INT_H
+typedef struct {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed)) idt_ptr_t;
+
+typedef struct {
+	uint32_t cr2;
+	uint32_t ds;
+	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	uint32_t int_no, err_code;
+	uint32_t eip, cs, eflags, useresp, ss;
+} interrupt_regs;
+
+void set_idt_gate(int n, uint32_t handler);
+void init_idt();
+void isr_handler(interrupt_regs *r);
+void irq_handler();
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+extern void isr128();
+extern void isr177();
+
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
+#endif // ARCH_I386_IDT_H
