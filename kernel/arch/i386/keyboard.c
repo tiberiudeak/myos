6de107203b2a484bc1817fea9d0816dedc9b6429
Tiberiu Deak <tiberiudeak09@gmail.com>
2024-02-13 07:50:27 +0200

    add shift key detection

--- kernel/arch/i386/keyboard.c
+++ kernel/arch/i386/keyboard.c
@@ -4,11 +4,37 @@
 #include <stdio.h>
 
 void keyboard_handler() {
+
+	/* TODO: check what scancode to use */
+
+	/* scancode 1 */
+	key_info_t *key_info = (key_info_t*) KEY_INFO_ADDRESS;
+	key_info->scancode = 0;
+
 	uint8_t scancode = port_byte_in(PS2_DATA_PORT);
-	char c[2];
-	c[0] = scancode_map[scancode];
-	c[1] = '\0';
-	printf("%s", c);
+
+	if (scancode) {
+		if (scancode == LSH || scancode == RSH) {
+			key_info->shift = 1;
+		}
+		else if (scancode == LSH_RELEASE || scancode == RSH_RELEASE) {
+			key_info->shift = 0;
+		}
+		else {
+			if (key_info->shift) {
+				scancode = scancode_map_shifted[scancode];
+			}
+			else {
+				scancode = scancode_map[scancode];
+			}
+
+			// send key info to shell
+			char c[2];
+			c[0] = scancode;
+			c[1] = '\0';
+			printf("%s", c);
+		}
+	}
 }
 
 void keyboard_init() {
--- kernel/include/kernel/keyboard.h
+++ kernel/include/kernel/keyboard.h
@@ -3,16 +3,25 @@
 
 #include <arch/i386/irq.h>
 
-#define INV		0x00
-#define ESC		0x1B
-#define LCTRL	0x00
-#define RCTRL	0x00
-#define LSH		0x00
-#define RSH		0x00
-#define LALT	0x00
-#define RALT	0x00
-#define CAPS	0x00
+#define INV					0x00
+#define ESC					0x1B
+#define LCTRL				0x1D
+#define RCTRL				0xE0
+#define LSH					0x2A
+#define RSH					0x36
+#define LALT				0x00
+#define RALT				0x00
+#define CAPS				0x3A
 
+#define LSH_RELEASE			0xAA
+#define RSH_RELEASE			0xB6
+
+/* there is no memory allocator yet, so the
+key information will be stored at the fixed address 0x500
+*/
+#define KEY_INFO_ADDRESS	0x500
+
+/* scancode 1 */
 static uint8_t scancode_map[] =
 {
 	INV, ESC, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
@@ -28,6 +37,31 @@ static uint8_t scancode_map[] =
 	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV,
 	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV
 };
+
+static uint8_t scancode_map_shifted[] =
+{
+	INV, ESC, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
+	'\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', LCTRL,
+	'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', LSH, '|',
+	'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', RSH, '*', LALT, ' ', CAPS,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, '7', '8', '9',
+	'-', '4', '5', '6', '+', '1', '2', '3', '0', '.', INV, INV, INV, INV, INV,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV,
+	INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV, INV
+};
+
+/* TODO: add scancode 2 and 3 */
+
+typedef struct {
+	uint8_t scancode;
+	uint8_t shift;
+	uint8_t ctrl;
+	uint8_t caps;
+} __attribute__((packed)) key_info_t;
 
 void keyboard_handler();
 void keyboard_init(void);
